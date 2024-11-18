d1d8f44fd08023a152bcf531839d46fd27ef9122
Chayan Sarkar <131506479+Chayan0099@users.noreply.github.com>
2024-11-18 21:00:57 +0530

    started attribute comp

---
+++ my-code/app/components/Attributes.tsx
@@ -0,0 +1,7 @@
+export default function Attributes() {
+    return <div className="mt-4 grid grid-cols-10 h-full lg:max-w-[60%] self-center"> 
+        <div className="row-span-full bg-gradient-to-br from-cyan-500 to-blue-500 rounded-lg col-span-7 col-start-2 h-64 shadow-lg shadow-blue-500/50 sm:row-span-full sm:col-span-5 sm:col-start-2" >img</div>
+        <div className="self-start w-[80%] bg-gradient-to-br from-orange-500 to-yellow-500 rounded-lg col-span-7 col-start-4 text-white font-bold text-xl py-2 px-2 mt-4 shadow-lg shadow-orange-500/50 sm:row-span-full sm:col-span-6 sm:col-start-6">Why Choose Us?</div>
+        <div className="col-span-2 col-start-7 self-start mt-16"> hello </div>
+    </div>
+}
--- my-code/app/page.tsx
+++ my-code/app/page.tsx
@@ -1,8 +1,10 @@
+import Attributes from "./components/Attributes";
 import Topbar from "./components/Topbar"
 import './globals.css'; 
 
 export default function Home() {
  return <div>
     <Topbar></Topbar>
+    <Attributes></Attributes>
  </div>
 }
